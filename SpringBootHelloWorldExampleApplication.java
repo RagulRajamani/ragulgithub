a7dc5c2ff2639a46516f891e4b87f16a22275030
Ragul <ragulcse91@gmail.com>
2021-05-14 12:49:21 +0530

    commit 1249

--- SpringBootHelloWorldExampleApplication.java
+++ SpringBootHelloWorldExampleApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHelloWorldExampleApplication
 {
+    System.out.println("hi")
 public static void main(String[] args) 
 {
+  System.out.println("hello...")
 SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+  System.out.println("hello1...")
 }
 }
