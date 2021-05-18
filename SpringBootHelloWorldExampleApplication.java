21b3a0e04b8a1d5b43d67171524767d7448cdad0
Ragul <ragulcse91@gmail.com>
2021-05-18 09:39:46 +0530

    commit 18

--- SpringBootHelloWorldExampleApplication.java
+++ SpringBootHelloWorldExampleApplication.java
@@ -10,6 +10,7 @@ public class SpringBootHelloWorldExampleApplication
     public static void main(String[] args)
     {
         System.out.println("hello...")
+
         SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
         System.out.println("hello1...")
     }
